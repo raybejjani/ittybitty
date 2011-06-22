350d3d30648354c864f4988b0e298c3d28d85606
Ray Bejjani <ray.bejjani@gmail.com>
2011-06-22 11:11:02 -0700

    Fixed benchmark reference to simpleserve binary

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-./simpleserve/simpleserve > /dev/null &
+./examples/simpleserve/simpleserve > /dev/null &
 ./siege/bin/siege localhost:3490 -b -r100 --log=siege.log --rc=./siege/etc/siegerc -c10 > /dev/null
 killall simpleserve
 
